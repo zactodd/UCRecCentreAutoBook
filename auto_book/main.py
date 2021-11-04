3289427265c9b794378c7ffd7d11f86bd9f81776
Zac Todd <zactodd0@gmail.com>
2021-11-05 12:49:47 +1300

    Update main.py

--- auto_book/main.py
+++ auto_book/main.py
@@ -1,10 +1,9 @@
 from auto_book import utils
 import json
 import datetime
+import calendar
 
 if __name__ == "__main__":
-    booked_today = False
-
     with open('classes_to_book.json') as f:
         classes_to_book = json.load(f)
 
@@ -14,16 +13,16 @@ if __name__ == "__main__":
         # Block until the next opening time
         while now < opening:
             now = datetime.datetime.now()
-        day = now.weekday()
 
+        day = calendar.day_name[(now + utils.OPENING_DELTA).weekday()].lower()
         if day in classes_to_book:
             bookings = classes_to_book[day]
+
             opening_classes = utils.today_opening_classes()
 
             # Book classes
             for booking in bookings:
                 slot_pk = (booking['class'], booking['time'])
                 if slot_pk in opening_classes:
-                    utils.book(utils.USER_ID, opening_classes[slot_pk], booking['time'])
-
+                    utils.book(utils.USER_ID, opening_classes[slot_pk], int(now.strftime('%Y%m%d')))
         opening += datetime.timedelta(days=1)
