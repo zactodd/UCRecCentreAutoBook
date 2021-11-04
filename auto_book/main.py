5b2f80a4d91ae3644c0f88486e7125b924d7c124
Zac Todd <zactodd0@gmail.com>
2021-11-05 11:30:36 +1300

    Create main.py

---
+++ auto_book/main.py
@@ -0,0 +1,25 @@
+from auto_book import utils
+import datetime
+
+if __name__ == "__main__":
+    booked_today = False
+    classes_to_book = {}
+    now = datetime.datetime.now()
+    opening = utils.opening(now)
+    while True:
+        # Block until the next opening time
+        while now < opening:
+            now = datetime.datetime.now()
+        day = now.weekday()
+
+        if day in classes_to_book:
+            bookings = classes_to_book[day]
+            opening_classes = utils.today_opening_classes()
+
+            # Book classes
+            for booking in bookings:
+                slot_pk = (booking['class'], booking['time'])
+                if slot_pk in opening_classes:
+                    utils.book(utils.USER_ID, opening_classes[slot_pk], booking['time'])
+
+        opening += datetime.timedelta(days=1)
