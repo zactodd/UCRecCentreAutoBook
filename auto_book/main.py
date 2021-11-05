4a8d3e81d221d252218108813968e292771f448a
Zac Todd <zactodd0@gmail.com>
2021-11-06 11:37:18 +1300

    Update main.py

--- auto_book/main.py
+++ auto_book/main.py
@@ -10,6 +10,7 @@ FACILITY_ID = 'dbf12cf8-3674-4daf-903b-2cead0b7ece1'
 FACILITY_QUERY = f'Search?eventTypes=Class&facilityId={FACILITY_ID}'
 BOOKING_QUERY = 'Book'
 LOGIN_QUERY = 'Login'
+
 OPENING_DELTA = datetime.timedelta(days=5)
 
 HEADERS = {
@@ -25,6 +26,8 @@ HEADERS = {
     'x-mwapps-client': 'enduserweb',
     'x-mwapps-clientversion': '1.3.3-1096,enduserweb'
 }
+
+CLASSES_TO_BOOK = __file__[:__file__.rindex('/') + 1] + 'classes_to_book.json'
 
 
 def get_classes_between_dates(date_from, date_to):
@@ -65,31 +68,43 @@ def login(username, password):
 
 
 if __name__ == "__main__":
-
-    username = input("username (Email): ")
-    password = getpass.getpass("Password: ")
-
-    with open('classes_to_book.json') as f:
+    # Get classes
+    with open(CLASSES_TO_BOOK) as f:
         classes_to_book = json.load(f)
+
+    valid_login = False
+    while not valid_login:
+        try:
+            username = input('Username (Email): ')
+            password = getpass.getpass('Password: ')
+            facility_id, token = login(username, password)
+            valid_login = True
+        except:
+            print('Invalid username or password.')
+    print('Login successful.')
+
     now = datetime.datetime.now()
     opening = now.replace(hour=6, minute=1)
 
     while True:
-        # Block until the next opening time
-        while now < opening:
-            now = datetime.datetime.now()
+        try:
+            # Block until the next opening time
+            while now < opening:
+                now = datetime.datetime.now()
 
-        user_id, token = login(username, password)
+            user_id, token = login(username, password)
 
-        day = calendar.day_name[(now + OPENING_DELTA).weekday()].lower()
-        if day in classes_to_book:
-            bookings = classes_to_book[day]
-            opening_classes = today_opening_classes()
+            day = calendar.day_name[(now + OPENING_DELTA).weekday()].lower()
+            if day in classes_to_book:
+                bookings = classes_to_book[day]
+                opening_classes = today_opening_classes()
 
-            # Book classes
-            for booking in bookings:
-                slot_pk = (booking['class'], booking['time'])
-                if slot_pk in opening_classes:
-                    time_str = int((now + OPENING_DELTA).strftime('%Y%m%d'))
-                    book(user_id, opening_classes[slot_pk], time_str, token)
-        opening += datetime.timedelta(days=1)
+                # Book classes
+                for booking in bookings:
+                    slot_pk = (booking['class'], booking['time'])
+                    if slot_pk in opening_classes:
+                        time_str = int((now + OPENING_DELTA).strftime('%Y%m%d'))
+                        book(user_id, opening_classes[slot_pk], time_str, token)
+            opening += datetime.timedelta(days=1)
+        except Exception as e:
+            print(f'Error {e}')
