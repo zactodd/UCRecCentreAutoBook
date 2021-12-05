02e3ab9a9efb1c03ad33854b6e62e229f7af15f2
Zac Todd <zactodd0@gmail.com>
2021-12-06 01:43:55 +1300

    tidy

--- auto_book/main.py
+++ auto_book/main.py
@@ -37,7 +37,13 @@ HEADERS = {
 CLASSES_TO_BOOK = __file__[:__file__.rindex('/' if "/" in __file__ else '\\') + 1] + 'classes_to_book.json'
 
 
-def get_classes_between_dates(date_from, date_to):
+def classes_between_dates(date_from, date_to):
+    """
+    GEt all class info between two dates.
+    :param date_from: The date to start from.
+    :param date_to: The date to end at.
+    :return: A list of tuples containing the (name, datetime, id) of the classes between the two dates.
+    """
     date_from = date_from.strftime('%Y-%m-%d')
     date_to = date_to.strftime('%Y-%m-%d')
     url = f'{URL}{FACILITY_QUERY}&fromDate={date_from}&toDate={date_to}'
@@ -47,84 +53,116 @@ def get_classes_between_dates(date_from, date_to):
             for i in classes_info]
 
 
-def random_date_between(start_date, end_date):
-    delta = end_date - start_date
+def random_date_between(date_from, date_to):
+    """
+    Get a random date between two dates.
+    :param date_from: The date to start from.
+    :param date_to: The date to end at.
+    :return: A datetime between the two dates.
+    """
+    delta = date_to - date_from
     int_delta = (delta.days * 86400) + delta.seconds
     random_second = random.randint(0, int_delta)
-    return start_date + datetime.timedelta(seconds=random_second)
+    return date_from + datetime.timedelta(seconds=random_second)
 
 
 def today_opening_classes():
+    """
+    Get all classes that are open today.
+    :return: A list of tuples containing the (name, datetime, id) of the classes today.
+    """
     today = datetime.datetime.now()
     opening = today + OPENING_DELTA
     opening_tomorrow = opening + datetime.timedelta(days=1)
-    return get_classes_between_dates(opening, opening_tomorrow)
+    return classes_between_dates(opening, opening_tomorrow)
 
 
-def book(user_id, class_id, dt, token):
+def book(user_id, class_id, token, date):
+    """
+    Book a class.
+    :param user_id: The id of the user booking.
+    :param class_id: The id of the class to book.
+    :param date: A string representing the date to book the class.
+    :param token: The authentication token for the session.
+    """
     headers = HEADERS.copy()
     headers['authorization'] = f'Bearer {token}'
     requests.post(
         headers=headers,
         url=f'{URL}{BOOKING_QUERY}',
-        json={'userId': user_id, 'classId': class_id, 'partitionDate': dt},
+        json={'userId': user_id, 'classId': class_id, 'partitionDate': date},
     )
 
 
 def login(username, password):
+    """
+    Log in to the MyWellness website.
+    :param username: User's username.
+    :param password: User's password.
+    :return: The user's id and authentication token for the session.
+    """
     response = requests.post(
         headers=HEADERS,
         url=f'{SERVICE_URL}/{LOGIN_QUERY}',
         json={"username": username, "password": password, "keepMeLoggedIn": False},
     )
     login_info = json.loads(response.text)
-    return login_info['data']['userContext']['id'], login_info['token']
+    return login_info['data']['userContext']['id'], login_info['token'] if login_info else None
 
 
 def book_classes_today(username, password, bookings, tol=TOLERANCE):
+    """
+    Book classes for today.
+    :param username: User's username.
+    :param password: User's password.
+    :param bookings: The classes to book.
+    :param tol: The tolerance of the booking times.
+    """
     user_id, token = login(username, password)
     date_str = int((now + OPENING_DELTA).strftime(DATE_FORMAT))
     for n, dt, idx in today_opening_classes():
         ct = dt.time()
         if any(n == m and bdt.time() - tol <= ct <= bdt.time() + tol for m, bdt in bookings):
-            book(user_id, idx, date_str, token)
+            book(user_id, idx, token, date_str)
 
 
 if __name__ == "__main__":
     # Validate login info
-    valid_login = False
-    while not valid_login:
-        try:
-            username = input('Username (Email): ')
-            password = getpass.getpass('Password: ')
-            facility_id, token = login(username, password)
-            valid_login = True
-        except:
+    while True:
+        username = input('Username (Email): ')
+        password = getpass.getpass('Password: ')
+        if login(username, password):
+            break
+        else:
             print('Invalid username or password.')
     print('Login successful')
 
+    # Get opening time
     now = datetime.datetime.now()
     opening = now.replace(hour=6, minute=0, second=0, microsecond=0)
     opening = random_date_between(opening + ONE_MINUTE, opening + TEN_MINUTES)
     while True:
         try:
+            # Block until the next opening time
+            while now < opening:
+                now = datetime.datetime.now()
+
+            # Get classes to book
             with open(CLASSES_TO_BOOK) as f:
                 bookings_json = json.load(f)
             daily_bookings = {d: [(i['class'], datetime.datetime.strptime(i['time'], TIME_FORMAT)) for i in info]
                               for d, info in bookings_json.items()}
-
-            # Block until the next opening time
-            while now < opening:
-                now = datetime.datetime.now()
 
             # Book classes opening today
             day = calendar.day_name[(now + OPENING_DELTA).weekday()].lower()
             if day in daily_bookings:
                 book_classes_today(username, password, daily_bookings[day])
 
+            # Push opening to next opening time
             opening += datetime.timedelta(days=1)
             opening = opening.replace(hour=6, minute=0, second=0, microsecond=0)
             opening = random_date_between(opening + ONE_MINUTE, opening + TEN_MINUTES)
+
         except json.JSONDecodeError as e:
             print(f'Cannot read file at {CLASSES_TO_BOOK}. {e}')
         except FileExistsError as e:
