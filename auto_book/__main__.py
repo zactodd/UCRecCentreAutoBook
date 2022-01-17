9f92f6db2f115bfc6f4899ea28524669f5569094
Zac Todd <zactodd0@gmail.com>
2022-01-18 10:44:11 +1300

    Update __main__.py

--- auto_book/__main__.py
+++ auto_book/__main__.py
@@ -12,8 +12,6 @@ parser.add_argument('-f', '--bookings', type=str,
                     help='File path of class to book json.', default=utils.CLASSES_TO_BOOK)
 parser.add_argument('-t', '--tolerance',
                     type=int, help='Tolerance between booking time and class time.', default=90)
-parser.add_argument('-r', '--repeat_daily',
-                    type=bool, help='If to repeat the script daily.', default=False, const=True, nargs='?')
 parser.add_argument('-d', '--random_delay',
                     type=bool, help='If to randomly delay the bookings.', default=False, const=True, nargs='?')
 
@@ -24,19 +22,14 @@ if __name__ == "__main__":
     username, password = kwargs['username'], kwargs['password']
     assert utils.login(username, password), 'Invalid username or password.'
 
-    repeat_booking = kwargs['repeat_daily']
     booking_file = kwargs['bookings']
     random_delay = kwargs['random_delay']
     tolerance = timedelta(minutes=kwargs['tolerance'])
 
     # Opening time
-    openings = accumulate(repeat(timedelta(days=1)), initial=datetime.now().replace(hour=6, minute=0, second=0))
+    opening = datetime.now().replace(hour=6, minute=0, second=0)
     if random_delay:
-        openings = map(lambda dt: utils.random_date_between(dt + utils.ONE_MINUTE, dt + utils.THREE_MINUTES), openings)
+        opening = utils.random_date_between(opening + utils.ONE_MINUTE, opening + utils.THREE_MINUTES)
 
     # Book classes on opening
-    utils.book_class_on_opening(username, password, next(openings), booking_file, tolerance)
-    
-    if repeat_booking:
-        for opening in openings:
-            utils.book_class_on_opening(username, password, opening, booking_file, tolerance)
+    utils.book_class_on_opening(username, password, opening, booking_file, tolerance)
