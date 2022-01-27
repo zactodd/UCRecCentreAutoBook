76b87f17245c6e70381517e9341414c925afd7a7
Zac Todd <zactodd0@gmail.com>
2022-01-28 10:50:55 +1300

    Update booking.py

--- auto_book/booking.py
+++ auto_book/booking.py
@@ -195,7 +195,7 @@ def special_classes(look_back_interval=utils.FORTNIGHT):
     classes = classes_between_dates(yesterday - look_back_interval, yesterday)
     classes = {name for name, *_ in classes}
     today_classes = today_opening_classes()
-    return [c for c in today_classes if c[0] in classes]
+    return [c for c in today_classes if c[0] not in classes]
 
 
 def book_special_classes(username, password):
